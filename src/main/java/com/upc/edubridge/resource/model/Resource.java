98cd7233e300cdbc8f1179263d6ee5a349850ee0
Patricia Rojas Sánchez <patriciarojasssanchez@gmail.com>
2026-05-05 19:56:30 -0500

    feat(resource): agregar entidad Resource para gestión de materiales académicos

--- src/main/java/com/upc/edubridge/resource/model/Resource.java
+++ src/main/java/com/upc/edubridge/resource/model/Resource.java
@@ -1,4 +1,25 @@
 package com.upc.edubridge.resource.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "resources")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Resource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private String title;
+    private String subject;
+    private String meta;
+    private String stats;
+    private Double rating;
+    private String img;
 }
