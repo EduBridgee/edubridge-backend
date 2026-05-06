a96f5d2f96885f8bb8bf8402bbee0db38e2a7a05
Carl0sFabian <fabian.mendoza.quispe@gmail.com>
2026-05-05 19:31:31 -0500

    feat: implementar estructura inicial del módulo de cursos

---
+++ src/main/java/com/upc/edubridge/course/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.upc.edubridge.course.repository;
+
+import com.upc.edubridge.course.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
