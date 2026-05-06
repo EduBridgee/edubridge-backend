10d04fe253c9dc9c9766623d013a9fce22e61692
Patricia Rojas Sánchez <patriciarojasssanchez@gmail.com>
2026-05-05 19:57:12 -0500

    feat(resource): agregar ResourceRepository con soporte JPA

--- src/main/java/com/upc/edubridge/resource/repository/ResourceRepository.java
+++ src/main/java/com/upc/edubridge/resource/repository/ResourceRepository.java
@@ -1,4 +1,5 @@
 package com.upc.edubridge.resource.repository;
+import com.upc.edubridge.resource.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResourceRepository {
-}
+public interface ResourceRepository extends JpaRepository<Resource, Long> {}
