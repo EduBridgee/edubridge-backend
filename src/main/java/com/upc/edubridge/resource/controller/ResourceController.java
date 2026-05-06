b9c562d7b54428ad80caa77aea016b45ea06b7c9
Patricia Rojas Sánchez <patriciarojasssanchez@gmail.com>
2026-05-05 19:58:11 -0500

    feat(resource): implementar controlador para listar y registrar recursos

--- src/main/java/com/upc/edubridge/resource/controller/ResourceController.java
+++ src/main/java/com/upc/edubridge/resource/controller/ResourceController.java
@@ -1,4 +1,26 @@
 package com.upc.edubridge.resource.controller;
 
+import com.upc.edubridge.resource.model.Resource;
+import com.upc.edubridge.resource.repository.ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/resources")
 public class ResourceController {
+
+    @Autowired
+    private ResourceRepository resourceRepository;
+
+    @GetMapping
+    public List<Resource> getAll() {
+        return resourceRepository.findAll();
+    }
+
+    @PostMapping
+    public Resource createResource(@RequestBody Resource resource) {
+        return resourceRepository.save(resource);
+    }
 }
